44d49b7bfd849ab82a21b213177f60108737b981
UdayAhire21 <udayahire27feb@gmail.com>
2025-12-21 19:23:13 +0530

    update

--- TwoSum.java
+++ TwoSum.java
@@ -13,4 +13,12 @@ public class TwoSum {
         }
         throw new IllegalArgumentException("No two sum solution");
     }
+
+    public static void main(String[] args) {
+        TwoSum ts = new TwoSum();
+        int[] nums = {2, 7, 11, 15};
+        int target = 17;
+        int[] result = ts.twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
 }
